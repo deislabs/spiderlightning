b43bf8b3081f91fa661dc72e222b628202779eb1
Jiaxiao Zhou <jiazho@microsoft.com>
2022-05-27 22:44:39 -0700

    cargo clippy
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,10 +3,10 @@ mod common;
 #[cfg(test)]
 mod kv_test {
     use anyhow::Result;
-    use kv_filesystem::KvFilesystem;
-    use runtime::Builder;
+    
+    
     use temp_dir::TempDir;
-    use url::Url;
+    
     use crate::common::run;
 
     const WASI_CLOUD_BINARY: &str = "./target/debug/wasi-cloud";
@@ -15,7 +15,7 @@ mod kv_test {
     #[test]
     fn test_kv_filesystem() -> Result<()> {
         let d = TempDir::new().unwrap();
-        let url = format!("file://{}", d.path().to_string_lossy().to_string());
+        let url = format!("file://{}", d.path().to_string_lossy());
         run(WASI_CLOUD_BINARY, vec!["-m", KV_TEST_MODULE, "-c", &url]);
         Ok(())
     }
