714738cc3cb4d2855380c82405ef6c397b6648a1
Dan Chiarlone <dchiarlone@microsoft.com>
2022-06-08 11:27:12 -0700

    addressing @Mossaka's PR comments
    Signed-off-by: Dan Chiarlone <dchiarlone@microsoft.com>

--- examples/lockd-demo/src/main.rs
+++ examples/lockd-demo/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 use lockd::*;
 wit_bindgen_rust::import!("../../wit/lockd.wit");
 
@@ -6,23 +8,31 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let lockd = get_lockd()?;
 
-    let leaseless_lock_key = lock(&lockd, "leaseless-lock".as_bytes())?;
+    let lock_with_time_to_live =
+        lock_with_time_to_live(&lockd, "lock_with_time_to_live".as_bytes(), 10)?;
     println!(
-        "made leaseless lock with the following key: {}",
-        std::str::from_utf8(&leaseless_lock_key)?
+        "made a lock with 10s to live that has the following key: {}",
+        std::str::from_utf8(&lock_with_time_to_live)?
     );
 
-    unlock(&lockd, &leaseless_lock_key)?;
-    println!("unlocked leaseless lock!");
-
-    let leasefull_lock_key = lock_with_time_to_live(&lockd, "leasefull-lock".as_bytes(), 60)?;
+    println!("trying to acquire the previously created lock...");
+    let now = SystemTime::now();
+    let early_acquire_lock = lock(&lockd, "lock_with_time_to_live".as_bytes())?;
     println!(
-        "made leasefull lock with the following key: {}",
-        std::str::from_utf8(&leasefull_lock_key)?
+        "managed to acquire lock after {:?}s, that lock's time to live was 10s",
+        now.elapsed()?.as_secs()
     );
+    unlock(&lockd, &early_acquire_lock)?;
+    println!("unlocked the lock we just acquried!");
 
-    unlock(&lockd, &leasefull_lock_key)?;
-    println!("unlocked leasefull lock!");
+    let lock_with_no_time_to_live = lock(&lockd, "lock_with_no_time_to_live".as_bytes())?;
+    println!(
+        "made a lock with no specific time to live that has the following key: {}",
+        std::str::from_utf8(&lock_with_no_time_to_live)?
+    );
+    unlock(&lockd, &lock_with_no_time_to_live)?;
+    println!("unlocked the lock we just acquried!");
+
     Ok(())
 }
 
