cd288270ca3d97218c8db35f280024075cef782a
Jiaxiao Zhou <jiazho@microsoft.com>
2022-06-29 00:17:05 -0700

    modified integration tests to fail-stop and added az blob storage tests
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,13 +5,26 @@ mod kv_test {
     use crate::common::run;
     use anyhow::Result;
 
-    const WASI_CLOUD_BINARY: &str = "./target/debug/wasi-cloud";
+    const WASI_CLOUD_BINARY: &str = "./target/release/wasi-cloud";
     const KV_TEST_MODULE: &str = "tests/kv-test/target/wasm32-wasi/debug/kv-test.wasm";
 
     #[test]
     fn test_kv_filesystem() -> Result<()> {
-        let config = "./tests/wc.toml";
-        run(WASI_CLOUD_BINARY, vec!["-m", KV_TEST_MODULE, "-c", config]);
+        let file_config = "./tests/file.toml";
+        run(
+            WASI_CLOUD_BINARY,
+            vec!["-m", KV_TEST_MODULE, "-c", file_config],
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_kv_azblob() -> Result<()> {
+        let azblob_config = "./tests/azblob.toml";
+        run(
+            WASI_CLOUD_BINARY,
+            vec!["-m", KV_TEST_MODULE, "-c", azblob_config],
+        );
         Ok(())
     }
 }
--- tests/kv-test/src/main.rs
+++ tests/kv-test/src/main.rs
@@ -3,40 +3,51 @@ use anyhow::Result;
 
 use kv::*;
 wit_bindgen_rust::import!("../../wit/kv.wit");
-wit_error_rs::impl_error!(Error);
 
-fn main() -> Result<()> {
+fn main() {
     // test get, set, delete
-    let rd = get_kv("rand")?; // TODO: this should be a random name
+    let rd = get_kv("rand").unwrap(); // TODO: this should be a random name
     let value = "wasi-cloud".as_bytes();
-    set(&rd, "key", value)?;
+    set(&rd, "key", value).unwrap();
     println!(
         "Hello, world! the value is: {}",
-        std::str::from_utf8(&get(&rd, "key")?)?
+        std::str::from_utf8(&get(&rd, "key").unwrap()).unwrap()
     );
-    delete(&rd, "key")?;
+    delete(&rd, "key").unwrap();
     let value = get(&rd, "key");
     assert!(value.is_err());
 
-    // test get_kv() will refer to the same underlying resource
-    let rd1 = get_kv("random1")?;
-    let rd2 = get_kv("random2")?;
-    set(&rd1, "key1", "value1".as_bytes())?;
-    set(&rd2, "key2", "value2".as_bytes())?;
+    // test get_kv() will have a unique allocation in the resource table.
+    // so two `get_kv()` with different names will return different allocations.
+    let rd1 = get_kv("random1").unwrap();
+    let rd2 = get_kv("random2").unwrap();
+    set(&rd1, "key1", "value1".as_bytes()).unwrap();
+    set(&rd2, "key2", "value2".as_bytes()).unwrap();
 
-    let value1 = get(&rd1, "key2")?;
-    let value2 = get(&rd2, "key1")?;
-    assert_eq!(std::str::from_utf8(&value1)?, "value2");
-    assert_eq!(std::str::from_utf8(&value2)?, "value1");
+    assert!(get(&rd1, "key2").is_err());
+    delete(&rd1, "key1").unwrap();
+    delete(&rd2, "key2").unwrap();
+
+    // test two get_kv() with the same name will return the same allocation.
+    // but the resource descriptors are not the same.
+    let rd1 = get_kv("random1").unwrap();
+    let rd2 = get_kv("random1").unwrap();
+    assert!(rd1 != rd2);
+    set(&rd1, "key1", "value1".as_bytes()).unwrap();
+    set(&rd2, "key2", "value2".as_bytes()).unwrap();
+    assert!(get(&rd1, "key2").unwrap() == "value2".as_bytes());
+    delete(&rd1, "key1").unwrap();
+    delete(&rd2, "key2").unwrap();
 
     // test get empty key
-    let rd = get_kv("random3")?;
+    let rd = get_kv("random3").unwrap();
     let value = get(&rd, "");
     assert!(value.is_err());
 
     // test delete empty key
-    let rd = get_kv("random4")?;
+    let rd = get_kv("random4").unwrap();
     let ret = delete(&rd, "key");
     assert!(ret.is_err());
-    Ok(())
+
+    println!("finished running kv-test");
 }
