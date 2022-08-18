aa6bd18ec4b86375177f8845505bcde1a5d9743f
Dan Chiarlone <dchiarlone@microsoft.com>
2022-08-18 15:05:07 -0700

    added retry logic
    Signed-off-by: Dan Chiarlone <dchiarlone@microsoft.com>

--- crates/runtime-configs/src/implementors/azapp.rs
+++ crates/runtime-configs/src/implementors/azapp.rs
@@ -4,6 +4,9 @@ use futures::executor::block_on;
 
 use super::envvars::EnvVars;
 
+// TODO: maybe make this configurable
+const MAX_NUM_RETRIES: i32 = 3;
+
 pub struct AzApp;
 
 fn make_client() -> Result<AzureAppConfigClient> {
@@ -17,25 +20,51 @@ fn make_client() -> Result<AzureAppConfigClient> {
 impl AzApp {
     pub fn get(key: &str) -> Result<Vec<u8>> {
         let app_config_client = make_client()?;
-        let res: String = block_on(app_config_client.get_key_value(key, SearchLabel::All))
-            .expect("failed to get key")
-            .value;
+        let mut ret = String::new();
+        let mut count = 0;
+        while count < MAX_NUM_RETRIES {
+            let res = block_on(app_config_client.get_key_value(key, SearchLabel::All));
+            if res.is_err() {
+                count += 1;
+            } else {
+                ret = res.unwrap().value;
+                // ^^^ ok to unwrap becasue we know res is not an err
+                break;
+            }
+        }
 
-        Ok(res.as_bytes().to_vec())
+        if count == MAX_NUM_RETRIES {
+            anyhow::bail!("failed to get message: maximum number of retries reached");
+        }
+
+        Ok(ret.as_bytes().to_vec())
     }
 
     pub fn set(key: &str, value: &[u8]) -> Result<()> {
         let app_config_client = make_client()?;
 
         tracing::debug!("attempting to set key...");
-        block_on(app_config_client.set_key(
-            key,
-            std::str::from_utf8(value)?,
-            SearchLabel::For(""),
-            None,
-            None,
-        ))
-        .expect("failed to set key value");
+
+        let mut count = 0;
+        while count < MAX_NUM_RETRIES {
+            let res = block_on(app_config_client.set_key(
+                key,
+                std::str::from_utf8(value)?,
+                SearchLabel::For(""),
+                None,
+                None,
+            ));
+
+            if res.is_err() {
+                count += 1;
+            } else {
+                break;
+            }
+        }
+
+        if count == MAX_NUM_RETRIES {
+            anyhow::bail!("failed to send message: maximum number of retries reached");
+        }
 
         Ok(())
     }
