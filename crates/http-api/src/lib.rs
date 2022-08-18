5b206b84bcc1dc986ee44e70ba98539788658cdc
Jiaxiao Zhou <jiazho@microsoft.com>
2022-08-18 16:58:05 -0700

    feat: added HTTP capability (#143)
    * http-api server starts, responds, and shuts down
    
    * register handler macro and http demo works
    
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>
    Co-authored-by: David Justice <david@devigned.com>
    Co-authored-by: Dan Chiarlone <dchiarlone@microsoft.com>

---
+++ crates/http-api/src/lib.rs
@@ -0,0 +1,165 @@
+use anyhow::{bail, Result};
+pub use http_handler::{Error, HttpHandler, HttpHandlerData, Method, Request, Response};
+use hyper::{
+    body::HttpBody as HyperHttpBody,
+    header::{HeaderName, HeaderValue},
+    Body, HeaderMap, StatusCode,
+};
+
+wit_bindgen_wasmtime::import!("../../wit/http-handler.wit");
+wit_error_rs::impl_error!(Error);
+
+/// A owned http_handler::HeadersParam
+///
+/// It can be directly transformed from `hyper::HeaderMap`
+/// ```rust
+/// let (parts, _) = request.into_parts();
+/// let header_map: HttpHeader = (&parts.headers).into()
+/// let req = http_handler::Request {
+///     headers: &headers.inner(),
+///     ...
+/// }
+/// ```
+pub struct HttpHeader<'a>(Vec<(&'a str, &'a str)>);
+
+impl<'a> HttpHeader<'a> {
+    pub fn inner(self) -> Vec<(&'a str, &'a str)> {
+        self.0
+    }
+}
+
+impl<'a> From<&'a hyper::HeaderMap> for HttpHeader<'a> {
+    fn from(headers: &'a hyper::HeaderMap) -> Self {
+        Self(
+            headers
+                .iter()
+                .map(|(name, value)| (name.as_str(), value.to_str().unwrap()))
+                .collect(),
+        )
+    }
+}
+
+/// An owned http_handler::BodyParam
+///
+/// It can be directly transformed from `hyper::Body`
+/// ```rust
+/// let (parts, body) = request.into_parts();
+/// let bytes = HttpBody::from_body(body).await?.inner();
+/// let req = http_handler::Request {
+///     body: Some(&bytes),
+///     ...
+/// }
+/// ```
+pub struct HttpBody(Vec<u8>);
+
+impl HttpBody {
+    pub async fn from_body(body: hyper::Body) -> Result<Self> {
+        const MAX_ALLOWED_SIZE: u64 = u64::MAX;
+        let content_length = match body.size_hint().upper() {
+            Some(v) => v,
+            None => bail!("HTTP Body too large"),
+        };
+
+        if content_length < MAX_ALLOWED_SIZE {
+            let body_bytes = hyper::body::to_bytes(body).await?;
+            let owned_body = Self(body_bytes.to_vec());
+            return Ok(owned_body);
+        }
+
+        bail!("HTTP body too large")
+    }
+
+    pub fn inner(self) -> Vec<u8> {
+        self.0
+    }
+}
+
+impl From<&hyper::Method> for Method {
+    fn from(method: &hyper::Method) -> Self {
+        match *method {
+            hyper::Method::GET => Method::Get,
+            hyper::Method::POST => Method::Post,
+            hyper::Method::PUT => Method::Put,
+            hyper::Method::DELETE => Method::Delete,
+            hyper::Method::PATCH => Method::Patch,
+            hyper::Method::HEAD => Method::Head,
+            hyper::Method::OPTIONS => Method::Options,
+            _ => panic!("unsupported method"),
+        }
+    }
+}
+
+impl From<Response> for hyper::Response<Body> {
+    fn from(res: Response) -> Self {
+        let mut response = if let Some(body) = res.body {
+            hyper::Response::new(Body::from(body))
+        } else {
+            hyper::Response::new(Body::empty())
+        };
+        *response.status_mut() = StatusCode::from_u16(res.status).unwrap();
+        if let Some(headers) = res.headers {
+            let headers = HeaderMap::from_iter(headers.iter().map(|(key, value)| {
+                (
+                    HeaderName::from_bytes(key.as_bytes()).unwrap(),
+                    HeaderValue::from_str(value).unwrap(),
+                )
+            }));
+            *response.headers_mut() = headers;
+        }
+        response
+    }
+}
+
+#[cfg(test)]
+mod unittests {
+
+    use crate::{HttpBody, HttpHeader};
+
+    use super::{Body, HeaderValue, Method, Request, Response, StatusCode};
+    use anyhow::Result;
+
+    #[tokio::test]
+    async fn test_request_conversion() -> Result<()> {
+        let req = hyper::Request::builder()
+            .method(hyper::Method::GET)
+            .uri("http://localhost:8080/")
+            .header("Content-Type", "application/json")
+            .body(hyper::Body::from("{\"name\": \"John\"}"))?;
+
+        let (parts, body) = req.into_parts();
+        let headers: HttpHeader = (&parts.headers).into();
+        let uri = &(&parts.uri).to_string();
+        let method: Method = (&parts.method).into();
+        let bytes: HttpBody = HttpBody::from_body(body).await?;
+        let params = [];
+        let req = Request {
+            method,
+            uri,
+            headers: &headers.0,
+            params: &params,
+            body: Some(&bytes.0),
+        };
+
+        assert_eq!(req.method, Method::Get);
+        assert_eq!(req.headers[0].1, "application/json");
+        assert_eq!(req.headers.len(), 1);
+        assert_eq!(req.body, Some(b"{\"name\": \"John\"}".to_vec().as_ref()));
+        assert_eq!(req.uri, "http://localhost:8080/");
+        Ok(())
+    }
+
+    #[test]
+    fn test_response_conversion() {
+        let res = Response {
+            status: 200,
+            headers: Some(vec![("Content-Type".into(), "text/plain".into())]),
+            body: Some("Hello World".into()),
+        };
+        let hyper_res: hyper::Response<Body> = res.into();
+        assert_eq!(hyper_res.status(), StatusCode::OK);
+        assert_eq!(
+            hyper_res.headers().get("Content-Type"),
+            Some(&HeaderValue::from_static("text/plain"))
+        );
+    }
+}
--- slight/src/commands/run.rs
+++ slight/src/commands/run.rs
@@ -1,19 +1,21 @@
+use std::sync::{Arc, Mutex};
+
 use anyhow::{bail, Result};
 use as_any::Downcast;
 use slight_events::{Events, EventsState};
 use slight_events_api::event_handler::EventHandler;
+use slight_http::{Http, HttpState};
 use slight_kv::{Kv, KvState};
 use slight_lockd::{Lockd, LockdState};
 use slight_mq::{Mq, MqState};
 use slight_pubsub::{Pubsub, PubsubState};
 use slight_runtime::{
-    resource::{BasicState, StateTable},
+    resource::{BasicState, Ctx, Resource, StateTable},
     Builder,
 };
 use slight_runtime_configs::{Configs, ConfigsState};
-use std::sync::{Arc, Mutex};
-
 use spiderlightning::core::slightfile::TomlFile;
+use wit_bindgen_wasmtime::wasmtime::Store;
 
 const KV_HOST_IMPLEMENTORS: [&str; 3] = ["kv.filesystem", "kv.azblob", "kv.awsdynamodb"];
 const MQ_HOST_IMPLEMENTORS: [&str; 2] = ["mq.filesystem", "mq.azsbus"];
@@ -21,93 +23,180 @@ const LOCKD_HOST_IMPLEMENTORS: [&str; 1] = ["lockd.etcd"];
 const PUBSUB_HOST_IMPLEMENTORS: [&str; 1] = ["pubsub.confluent_apache_kafka"];
 const CONFIGS_HOST_IMPLEMENTORS: [&str; 2] = ["configs.usersecrets", "configs.envvars"];
 
-pub fn handle_run(module: &str, toml: &TomlFile, toml_file_path: &str) -> Result<()> {
+pub async fn handle_run(module: &str, toml: &TomlFile, toml_file_path: &str) -> Result<()> {
     tracing::info!("Starting slight");
 
     let resource_map = Arc::new(Mutex::new(StateTable::default()));
 
-    let mut host_builder = Builder::new_default()?;
-    let mut guest_builder = Builder::new_default()?;
-    host_builder.link_wasi()?;
-    guest_builder.link_wasi()?;
-    let mut events_enabled = false;
+    let host_builder = build_store_instance(toml, toml_file_path, resource_map.clone())?;
+    let (_, mut store, instance) = host_builder.build(module)?;
+
+    let caps = toml.capability.as_ref().unwrap();
+    // looking for events capability.
+    let events_enabled = caps.iter().any(|cap| cap.name == "events");
+
+    // looking for http capability.
+    let http_enabled = caps.iter().any(|cap| cap.name == "http");
+
+    if events_enabled {
+        log::debug!("Events capability enabled");
+        let guest_builder = build_store_instance(toml, toml_file_path, resource_map.clone())?;
+        let (_, mut store2, instance2) = guest_builder.build(module)?;
+        let event_handler = EventHandler::new(&mut store2, &instance2, |ctx| &mut ctx.state)?;
+        let event_handler_resource: &mut Events = get_resource(&mut store, "events");
+        event_handler_resource.update_state(
+            Arc::new(Mutex::new(store2)),
+            Arc::new(Mutex::new(event_handler)),
+        )?;
+    }
+
+    if http_enabled {
+        log::debug!("Http capability enabled");
+        let guest_builder = build_store_instance(toml, toml_file_path, resource_map.clone())?;
+        let (_, store2, instance2) = guest_builder.build(module)?;
+        let http_api_resource: &mut Http = get_resource(&mut store, "http");
+        http_api_resource.update_state(
+            Arc::new(Mutex::new(store2)),
+            Arc::new(Mutex::new(instance2)),
+        )?;
+    }
+
+    tracing::info!("Executing {}", module);
+    instance
+        .get_typed_func::<(), _, _>(&mut store, "_start")?
+        .call(&mut store, ())?;
+
+    if http_enabled {
+        log::info!("waiting for http to finish...");
+        shutdown_signal().await;
+        let http_api_resource: &mut Http = get_resource(&mut store, "http");
+        http_api_resource.close();
+    }
+    Ok(())
+}
+
+fn get_resource<'a, T>(store: &'a mut Store<Ctx>, scheme_name: &'a str) -> &'a mut T
+where
+    T: Resource,
+{
+    let err_msg = format!(
+        "internal error: resource_map does not contain key: {}",
+        scheme_name
+    );
+    let err_msg2 = format!(
+        "internal error: resource map contains key {} but can't downcast",
+        scheme_name
+    );
+    store
+        .data_mut()
+        .data
+        .get_mut(scheme_name)
+        .expect(&err_msg)
+        .0
+        .as_mut()
+        .downcast_mut::<T>()
+        .expect(&err_msg2)
+}
+
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
+fn build_store_instance(
+    toml: &TomlFile,
+    toml_file_path: &str,
+    resource_map: Arc<Mutex<StateTable>>,
+) -> Result<Builder> {
+    let mut builder = Builder::new_default()?;
+    builder.link_wasi()?;
     if toml.specversion.as_ref().unwrap() == "0.1" {
         for c in toml.capability.as_ref().unwrap() {
             let resource_type: &str = c.name.as_str();
             match resource_type {
-            "events" => {
-                events_enabled = true;
-                host_builder.link_capability::<Events>(resource_type.to_string(), EventsState::new(resource_map.clone()))?;
-                guest_builder.link_capability::<Events>(resource_type.to_string(), EventsState::new(resource_map.clone()))?;
-            },
-            _ if KV_HOST_IMPLEMENTORS.contains(&resource_type) => {
-                if let Some(ss) = &toml.secret_store {
-                    host_builder.link_capability::<Kv>("kv".to_string(), KvState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                    guest_builder.link_capability::<Kv>("kv".to_string(), KvState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                } else {
-                    bail!("the kv capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab, say, the AZURE_STORAGE_ACCOUNT, and AZURE_STORAGE_KEY from.")
+                "events" => {
+                    builder.link_capability::<Events>(
+                        resource_type.to_string(),
+                        EventsState::new(resource_map.clone()),
+                    )?;
                 }
-            },
-            _ if MQ_HOST_IMPLEMENTORS.contains(&resource_type) => {
-                if let Some(ss) = &toml.secret_store {
-                    host_builder.link_capability::<Mq>("mq".to_string(), MqState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                    guest_builder.link_capability::<Mq>("mq".to_string(), MqState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                } else {
-                    bail!("the mq capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab the AZURE_SERVICE_BUS_NAMESPACE, AZURE_POLICY_NAME, and AZURE_POLICY_KEY from.")
+                _ if KV_HOST_IMPLEMENTORS.contains(&resource_type) => {
+                    if let Some(ss) = &toml.secret_store {
+                        builder.link_capability::<Kv>(
+                            "kv".to_string(),
+                            KvState::new(
+                                resource_type.to_string(),
+                                BasicState::new(resource_map.clone(), ss, toml_file_path),
+                            ),
+                        )?;
+                    } else {
+                        bail!("the kv capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab, say, the AZURE_STORAGE_ACCOUNT, and AZURE_STORAGE_KEY from.")
+                    }
+                }
+                _ if MQ_HOST_IMPLEMENTORS.contains(&resource_type) => {
+                    if let Some(ss) = &toml.secret_store {
+                        builder.link_capability::<Mq>(
+                            "mq".to_string(),
+                            MqState::new(
+                                resource_type.to_string(),
+                                BasicState::new(resource_map.clone(), ss, toml_file_path),
+                            ),
+                        )?;
+                    } else {
+                        bail!("the mq capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab the AZURE_SERVICE_BUS_NAMESPACE, AZURE_POLICY_NAME, and AZURE_POLICY_KEY from.")
+                    }
+                }
+                _ if LOCKD_HOST_IMPLEMENTORS.contains(&resource_type) => {
+                    if let Some(ss) = &toml.secret_store {
+                        builder.link_capability::<Lockd>(
+                            "lockd".to_string(),
+                            LockdState::new(
+                                resource_type.to_string(),
+                                BasicState::new(resource_map.clone(), ss, toml_file_path),
+                            ),
+                        )?;
+                    } else {
+                        bail!("the lockd capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab the ETCD_ENDPOINT.")
+                    }
+                }
+                _ if PUBSUB_HOST_IMPLEMENTORS.contains(&resource_type) => {
+                    if let Some(ss) = &toml.secret_store {
+                        builder.link_capability::<Pubsub>(
+                            "pubsub".to_string(),
+                            PubsubState::new(
+                                resource_type.to_string(),
+                                BasicState::new(resource_map.clone(), ss, toml_file_path),
+                            ),
+                        )?;
+                    } else {
+                        bail!("the mq capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab the AZURE_SERVICE_BUS_NAMESPACE, AZURE_POLICY_NAME, and AZURE_POLICY_KEY from.")
+                    }
+                }
+                _ if CONFIGS_HOST_IMPLEMENTORS.contains(&resource_type) => {
+                    builder.link_capability::<Configs>(
+                        "configs".to_string(),
+                        ConfigsState::new(
+                            resource_type.to_string(),
+                            BasicState::new(resource_map.clone(), "", toml_file_path),
+                        ),
+                    )?;
+                }
+                "http" => {
+                    builder.link_capability::<Http>(
+                        resource_type.to_string(),
+                        HttpState::new(resource_map.clone()),
+                    )?;
+                }
+                _ => {
+                    bail!("invalid url: currently slight only supports 'configs.usersecrets', 'configs.envvars', 'events', 'kv.filesystem', 'kv.azblob', 'kv.awsdynamodb', 'mq.filesystem', 'mq.azsbus', 'lockd.etcd', 'pubsub.confluent_apache_kafka', and 'http' schemes")
                 }
             }
-            _ if LOCKD_HOST_IMPLEMENTORS.contains(&resource_type) => {
-                if let Some(ss) = &toml.secret_store {
-                    host_builder.link_capability::<Lockd>("lockd".to_string(), LockdState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                    guest_builder.link_capability::<Lockd>("lockd".to_string(),LockdState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                } else {
-                    bail!("the lockd capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab the ETCD_ENDPOINT.")
-                }
-            },
-            _ if PUBSUB_HOST_IMPLEMENTORS.contains(&resource_type) => {
-                if let Some(ss) = &toml.secret_store {
-                    host_builder.link_capability::<Pubsub>("pubsub".to_string(), PubsubState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                    guest_builder.link_capability::<Pubsub>("pubsub".to_string(), PubsubState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), ss, toml_file_path)))?;
-                } else {
-                    bail!("the pubsub capability requires a secret store of some type (i.e., envvars, or usersecrets) specified in your config file so it knows where to grab the CK_SECURITY_PROTOCOL, CK_SASL_MECHANISMS, CK_SASL_USERNAME, CK_SASL_PASSWORD, and CK_GROUP_ID.")
-                }
-            },
-            _ if CONFIGS_HOST_IMPLEMENTORS.contains(&resource_type) => {
-                host_builder.link_capability::<Configs>(
-                    "configs".to_string(),
-                    ConfigsState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), "", toml_file_path)))?;
-                guest_builder.link_capability::<Configs>(
-                    "configs".to_string(),
-                    ConfigsState::new(resource_type.to_string(), BasicState::new(resource_map.clone(), "", toml_file_path)))?;
-            }
-            _ => bail!("invalid url: currently slight only supports 'configs.usersecrets', 'configs.envvars', 'events', 'kv.filesystem', 'kv.azblob', 'kv.awsdynamodb', 'mq.filesystem', 'mq.azsbus', 'lockd.etcd', and 'pubsub.confluent_apache_kafka' schemes"),
-        }
         }
     } else {
         bail!("unsupported toml spec version");
     }
 
-    let (_, mut store, instance) = host_builder.build(module)?;
-    let (_, mut store2, instance2) = guest_builder.build(module)?;
-    if events_enabled {
-        let event_handler = EventHandler::new(&mut store2, &instance2, |ctx| &mut ctx.state)?;
-        store
-            .data_mut()
-            .data
-            .get_mut("events")
-            .expect("internal error: resource_map does not contain key events")
-            .0
-            .as_mut()
-            .downcast_mut::<Events>()
-            .expect("internal error: resource map contains key events but can't downcast to Events")
-            .update_state(
-                Arc::new(Mutex::new(store2)),
-                Arc::new(Mutex::new(event_handler)),
-            )?;
-    }
-    tracing::info!("Executing {}", module);
-    instance
-        .get_typed_func::<(), _, _>(&mut store, "_start")?
-        .call(&mut store, ())?;
-    Ok(())
+    Ok(builder)
 }
