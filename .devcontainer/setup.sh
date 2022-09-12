18716d123aa47f2e3ba77c854885d018666d5b6b
Jiaxiao Zhou <jiazho@microsoft.com>
2022-09-12 10:14:59 -0700

    bump wasmtime to 0.39.1
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -29,7 +29,7 @@ rustup component add clippy --toolchain nightly
 
 cargo install cargo-expand
 cargo install cargo-edit
-cargo install --git https://github.com/bytecodealliance/wit-bindgen wit-bindgen-cli --rev a79a4be33d76ddf62839ba71602c26a96610ef7c
+cargo install --git https://github.com/bytecodealliance/wit-bindgen wit-bindgen-cli --rev cb871cfa1ee460b51eb1d144b175b9aab9c50aba
 rustup target add wasm32-wasi
 
 ## setup git
