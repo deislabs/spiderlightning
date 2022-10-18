0691b92bcafca4c1b0a05023d8cb2c2fb8163337
Jiaxiao Zhou <jiazho@microsoft.com>
2022-10-18 09:02:48 -0700

    added async to events cap (#220)
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>
    
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- crates/events-api/src/lib.rs
+++ crates/events-api/src/lib.rs
@@ -7,7 +7,7 @@ use std::sync::Mutex;
 // guest resource
 pub use event_handler::{EventHandler, EventHandlerData, EventParam};
 
-wit_bindgen_wasmtime::import!("../../wit/event-handler.wit");
+wit_bindgen_wasmtime::import!({paths: ["../../wit/event-handler.wit"], async: *});
 pub use cloudevents::AttributesReader;
 pub use cloudevents::AttributesWriter;
 pub use cloudevents::Event;
