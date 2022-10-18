a761c3fe26da9a29db1fd1bfab9dd0f65de70f99
Jiaxiao Zhou <jiazho@microsoft.com>
2022-10-18 11:04:41 -0700

    clippy
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- build.rs
+++ build.rs
@@ -18,18 +18,14 @@ fn main() {
     // Check if wasm32-wasi target is installed
     let target = "wasm32-wasi";
     let output = Command::new("rustup")
-        .args(&["target", "list", "--installed"])
+        .args(["target", "list", "--installed"])
         .output()
         .expect("failed to execute process");
     let output = std::str::from_utf8(&output.stdout).unwrap();
     if !output.lines().any(|line| line == target) {
-        writeln!(
-            stderr(),
-            "Error: {} target is not installed. Run `rustup target add {}`",
+        eprintln!("Error: {} target is not installed. Run `rustup target add {}`",
             target,
-            target
-        )
-        .unwrap();
+            target);
         std::process::exit(1);
     }
 
