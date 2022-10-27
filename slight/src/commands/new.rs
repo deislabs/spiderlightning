1462a850f3764af2ee7abc9c034c326f8e86648b
Jiaxiao Zhou <jiazho@microsoft.com>
2022-10-27 18:48:29 -0400

    feat(slight): better error message for slight new command (#228)
    * better error message for slight new command
    
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- slight/src/commands/new.rs
+++ slight/src/commands/new.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Context, Result};
 use flate2::bufread::GzDecoder;
 use std::io::Write;
 use std::{
@@ -23,13 +23,22 @@ pub async fn handle_new(name_at_release: &str, template: &Templates) -> Result<(
         (&name_at_release[..find_at], &name_at_release[find_at + 1..])
     };
 
+    // check project_name is not C or Rust
+    if project_name == "c" || project_name == "rust" {
+        bail!("project name cannot be c or rust");
+    }
+
     let resp = reqwest::get(format!(
         "https://github.com/deislabs/spiderlightning/releases/download/{}/{}-template.tar.gz",
         release, template
     ))
-    .await?
-    .bytes()
     .await?;
+    if resp.status() == 404 {
+        bail!("could not find template {} for release {}, pleases see all releases in https://github.com/deislabs/spiderlightning/releases", template, release);
+    }
+
+    let resp = resp.bytes().await?;
+
     Archive::new(GzDecoder::new(BufReader::new(resp.as_ref()))).unpack("./")?;
 
     match template {
@@ -55,7 +64,7 @@ fn setup_c_template(project_name: &str, release: &str) -> Result<()> {
     write!(makefile_f, "{}", makefile_s)?;
     drop(makefile_f);
 
-    std::fs::rename("c", project_name)?;
+    better_rename("c", project_name)?;
 
     Ok(())
 }
@@ -73,7 +82,12 @@ fn setup_rust_template(project_name: &str, release: &str) -> Result<()> {
     drop(cargo_f);
     drop(main_f);
 
-    std::fs::rename("rust", project_name)?;
+    better_rename("rust", project_name)?;
 
+    Ok(())
+}
+
+fn better_rename(from: &str, to: &str) -> Result<()> {
+    std::fs::rename(from, to).with_context(|| format!("could not create project: {}", to))?;
     Ok(())
 }
