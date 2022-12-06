d867f70bea8e47ec2200d4b39f1db245feac7d5e
danbugs <danilochiarlone@gmail.com>
2022-12-06 12:22:03 -0800

    updated the examples
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- examples/pubsub-consumer-demo/src/main.rs
+++ examples/pubsub-consumer-demo/src/main.rs
@@ -6,11 +6,21 @@ wit_error_rs::impl_error!(pubsub::Error);
 fn main() -> Result<()> {
     let ps = Sub::open("my-pubsub")?;
     let sub_tok = ps.subscribe("rust")?;
+    let sub_tok1 = ps.subscribe("global-chat")?;
+
     for _ in 0..3 {
         loop {
-            let msg = ps.receive(sub_tok)?;
+            let msg = ps.receive(&sub_tok)?;
             if !msg.is_empty() {
-                println!("received message> value: {:?}", String::from_utf8(msg));
+                println!("received message from topic 'rust'> value: {:?}", String::from_utf8(msg));
+                break;
+            }
+        }
+
+        loop {
+            let msg = ps.receive(&sub_tok1)?;
+            if !msg.is_empty() {
+                println!("received message from topic 'global-chat'> value: {:?}", String::from_utf8(msg));
                 break;
             }
         }
