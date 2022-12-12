2b69c1ff17ee2ec0ebbad13b854dd6482eecf0e6
danbugs <danilochiarlone@gmail.com>
2022-12-12 10:34:47 -0800

    updated pubsub
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- crates/pubsub/src/lib.rs
+++ crates/pubsub/src/lib.rs
@@ -11,36 +11,36 @@ use slight_common::{impl_resource, BasicState};
 /// It is mandatory to `use <interface>::*` due to `impl_resource!`.
 /// That is because `impl_resource!` accesses the `crate`'s
 /// `add_to_linker`, and not the `<interface>::add_to_linker` directly.
-use pubsub::*;
-wit_bindgen_wasmtime::export!({paths: ["../../wit/pubsub.wit"], async: *});
-wit_error_rs::impl_error!(pubsub::Error);
-wit_error_rs::impl_from!(anyhow::Error, pubsub::Error::ErrorWithDescription);
+use messaging::*;
+wit_bindgen_wasmtime::export!({paths: ["../../wit/messaging.wit"], async: *});
+wit_error_rs::impl_error!(messaging::MessagingError);
+wit_error_rs::impl_from!(anyhow::Error, messaging::MessagingError::UnexpectedError);
 wit_error_rs::impl_from!(
     std::string::FromUtf8Error,
-    pubsub::Error::ErrorWithDescription
+    messaging::MessagingError::UnexpectedError
 );
 
-/// The `Pubsub` structure is what will implement the `pubsub::Pubsub` trait
-/// coming from the generated code of off `pubsub.wit`.
+/// The `Messaging` structure is what will implement the `messaging::Messaging` trait
+/// coming from the generated code of off `messaging.wit`.
 ///
 /// It holds:
-///     - a `pubsub_implementor` `String` — this comes directly from a
+///     - a `messaging_implementor` `String` — this comes directly from a
 ///     user's `slightfile` and it is what allows us to dynamically
 ///     dispatch to a specific implementor's implentation, and
 ///     - the `slight_state` (of type `BasicState`) that contains common
 ///     things received from the slight binary (i.e., the `config_type`
 ///     and the `config_toml_file_path`).
 #[derive(Clone, Default)]
-pub struct Pubsub {
-    store: HashMap<String, PubsubState>,
+pub struct Messaging {
+    store: HashMap<String, MessagingState>,
 }
 
 #[derive(Clone, Debug)]
-struct PubsubState {
+struct MessagingState {
     pub_implementor: PubImplementor,
     sub_implementor: SubImplementor,
 }
-impl Pubsub {
+impl Messaging {
     pub async fn new(name: &str, capability_store: HashMap<String, BasicState>) -> Self {
         let state = capability_store.get(name).unwrap().clone();
 
@@ -54,7 +54,7 @@ impl Pubsub {
             .map(|c| {
                 (
                     c.0.clone(),
-                    PubsubState {
+                    MessagingState {
                         pub_implementor: p.clone(),
                         sub_implementor: s.clone(),
                     },
@@ -67,19 +67,19 @@ impl Pubsub {
 }
 
 impl_resource!(
-    Pubsub,
-    pubsub::PubsubTables<Pubsub>,
-    PubsubState,
-    pubsub::add_to_linker,
-    "pubsub".to_string()
+    Messaging,
+    messaging::MessagingTables<Messaging>,
+    MessagingState,
+    messaging::add_to_linker,
+    "messaging".to_string()
 );
 
 #[async_trait]
-impl pubsub::Pubsub for Pubsub {
+impl messaging::Messaging for Messaging {
     type Pub = PubImplementor;
     type Sub = SubImplementor;
 
-    async fn pub_open(&mut self, name: &str) -> Result<Self::Pub, Error> {
+    async fn pub_open(&mut self, name: &str) -> Result<Self::Pub, MessagingError> {
         let inner = self.store.get(name).unwrap().clone();
         Ok(inner.pub_implementor)
     }
@@ -87,9 +87,9 @@ impl pubsub::Pubsub for Pubsub {
     async fn pub_publish(
         &mut self,
         self_: &Self::Pub,
-        message: PayloadParam<'_>,
+        message: &[u8],
         topic: &str,
-    ) -> Result<(), Error> {
+    ) -> Result<(), MessagingError> {
         match &self_ {
             PubImplementor::ConfluentApacheKafka(pi) => pi.publish(message, topic)?,
             PubImplementor::Mosquitto(pi) => pi.publish(message, topic).await?,
@@ -99,7 +99,7 @@ impl pubsub::Pubsub for Pubsub {
         Ok(())
     }
 
-    async fn sub_open(&mut self, name: &str) -> Result<Self::Sub, Error> {
+    async fn sub_open(&mut self, name: &str) -> Result<Self::Sub, MessagingError> {
         let inner = self.store.get(name).unwrap().clone();
         Ok(inner.sub_implementor)
     }
@@ -108,7 +108,7 @@ impl pubsub::Pubsub for Pubsub {
         &mut self,
         self_: &Self::Sub,
         sub_tok: SubscriptionTokenParam<'_>,
-    ) -> Result<PayloadResult, Error> {
+    ) -> Result<Vec<u8>, MessagingError> {
         Ok(match &self_ {
             SubImplementor::ConfluentApacheKafka(pi) => pi.receive(sub_tok).await?,
             SubImplementor::Mosquitto(pi) => pi.receive(sub_tok).await?,
@@ -116,7 +116,7 @@ impl pubsub::Pubsub for Pubsub {
         })
     }
 
-    async fn sub_subscribe(&mut self, self_: &Self::Sub, topic: &str) -> Result<String, Error> {
+    async fn sub_subscribe(&mut self, self_: &Self::Sub, topic: &str) -> Result<String, MessagingError> {
         Ok(match &self_ {
             SubImplementor::ConfluentApacheKafka(pi) => pi.subscribe(topic).await?,
             SubImplementor::Mosquitto(pi) => pi.subscribe(topic).await?,
@@ -125,7 +125,7 @@ impl pubsub::Pubsub for Pubsub {
     }
 }
 
-/// This defines the available implementor implementations for the `Pubsub` interface.
+/// This defines the available implementor implementations for the `Messaging` interface.
 ///
 /// As per its' usage in `PubInner`, it must `derive` `Debug`, and `Clone`.
 #[derive(Debug, Clone, Default)]
@@ -137,12 +137,12 @@ pub enum PubImplementor {
 }
 
 impl PubImplementor {
-    async fn new(pubsub_implementor: &str, slight_state: &BasicState) -> Self {
-        match pubsub_implementor {
-            "pubsub.confluent_apache_kafka" => {
+    async fn new(messaging_implementor: &str, slight_state: &BasicState) -> Self {
+        match messaging_implementor {
+            "messaging.confluent_apache_kafka" => {
                 Self::ConfluentApacheKafka(apache_kafka::Pub::new(slight_state).await)
             }
-            "pubsub.mosquitto" => Self::Mosquitto(mosquitto::Pub::new(slight_state).await),
+            "messaging.mosquitto" => Self::Mosquitto(mosquitto::Pub::new(slight_state).await),
             p => panic!(
                 "failed to match provided name (i.e., '{}') to any known host implementations",
                 p
@@ -151,7 +151,7 @@ impl PubImplementor {
     }
 }
 
-/// This defines the available implementor implementations for the `Pubsub` interface.
+/// This defines the available implementor implementations for the `Messaging` interface.
 ///
 /// As per its' usage in `PubInner`, it must `derive` `Debug`, and `Clone`.
 #[derive(Debug, Clone, Default)]
@@ -163,12 +163,12 @@ pub enum SubImplementor {
 }
 
 impl SubImplementor {
-    async fn new(pubsub_implementor: &str, slight_state: &BasicState) -> Self {
-        match pubsub_implementor {
-            "pubsub.confluent_apache_kafka" => {
+    async fn new(messaging_implementor: &str, slight_state: &BasicState) -> Self {
+        match messaging_implementor {
+            "messaging.confluent_apache_kafka" => {
                 Self::ConfluentApacheKafka(apache_kafka::Sub::new(slight_state).await)
             }
-            "pubsub.mosquitto" => Self::Mosquitto(mosquitto::Sub::new(slight_state).await),
+            "messaging.mosquitto" => Self::Mosquitto(mosquitto::Sub::new(slight_state).await),
             p => panic!(
                 "failed to match provided name (i.e., '{}') to any known host implementations",
                 p
