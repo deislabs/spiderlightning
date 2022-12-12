4ebaa7ad463033b0134efb87f32468d29b4d6910
danbugs <danilochiarlone@gmail.com>
2022-12-12 09:26:49 -0800

    updated http tests
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- tests/http-test/src/main.rs
+++ tests/http-test/src/main.rs
@@ -4,7 +4,7 @@ use http::*;
 use slight_http_handler_macro::register_handler;
 
 wit_bindgen_rust::import!("../../wit/http.wit");
-wit_error_rs::impl_error!(Error);
+wit_error_rs::impl_error!(http::HttpError);
 
 fn main() -> Result<()> {
     let router = Router::new()?;
@@ -25,7 +25,7 @@ fn main() -> Result<()> {
 }
 
 #[register_handler]
-fn handle_hello(req: Request) -> Result<Response, Error> {
+fn handle_hello(req: Request) -> Result<Response, HttpError> {
     Ok(Response {
         headers: Some(req.headers),
         body: Some("hello".as_bytes().to_vec()),
@@ -34,7 +34,7 @@ fn handle_hello(req: Request) -> Result<Response, Error> {
 }
 
 #[register_handler]
-fn handle_person_with_name(req: Request) -> Result<Response, Error> {
+fn handle_person_with_name(req: Request) -> Result<Response, HttpError> {
     let id = req.params.into_iter().find(|x| x.0 == "name").unwrap_or(("".into(), "".into()));
     Ok(Response {
         headers: Some(req.headers),
@@ -44,7 +44,7 @@ fn handle_person_with_name(req: Request) -> Result<Response, Error> {
 }
 
 #[register_handler]
-fn handle_foo(request: Request) -> Result<Response, Error> {
+fn handle_foo(request: Request) -> Result<Response, HttpError> {
     Ok(Response {
         headers: Some(request.headers),
         body: request.body,
@@ -53,7 +53,7 @@ fn handle_foo(request: Request) -> Result<Response, Error> {
 }
 
 #[register_handler]
-fn handle_bar(request: Request) -> Result<Response, Error> {
+fn handle_bar(request: Request) -> Result<Response, HttpError> {
     assert_eq!(request.method, Method::Put);
     Ok(Response {
         headers: Some(request.headers),
@@ -63,7 +63,7 @@ fn handle_bar(request: Request) -> Result<Response, Error> {
 }
 
 #[register_handler]
-fn delete_file_handler(request: Request) -> Result<Response, Error> {
+fn delete_file_handler(request: Request) -> Result<Response, HttpError> {
     assert_eq!(request.method, Method::Delete);
     Ok(Response {
         headers: Some(request.headers),
@@ -74,7 +74,7 @@ fn delete_file_handler(request: Request) -> Result<Response, Error> {
 
 
 #[register_handler]
-fn upload(request: Request) -> Result<Response, Error> {
+fn upload(request: Request) -> Result<Response, HttpError> {
     assert_eq!(request.method, Method::Post);
     Ok(Response {
         headers: Some(request.headers),
@@ -87,8 +87,8 @@ fn upload(request: Request) -> Result<Response, Error> {
 /// handle_http function. The reason this is needed because the host is 
 /// parsing the wit htto-handler file which has the handle_http function.        
 #[register_handler]
-fn handle_http(_req: Request) -> Result<Response, Error> {
-    Err(Error::ErrorWithDescription(
+fn handle_http(_req: Request) -> Result<Response, HttpError> {
+    Err(HttpError::UnexpectedError(
         "this is a dummy handler".to_string(),
     ))
 }
