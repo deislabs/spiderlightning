e7a2834090f8477115a026c17c3e8a113d809982
Jiaxiao Zhou <jiazho@microsoft.com>
2022-12-29 23:45:50 -0800

    if rustup not found, proceed without failing in build script
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- build.rs
+++ build.rs
@@ -16,21 +16,24 @@ fn main() {
     println!("cargo:rerun-if-changed={CONFIGS_TEST_PATH}/src/main.rs");
 
     // Check if wasm32-wasi target is installed
+
     let target = "wasm32-wasi";
     let output = Command::new("rustup")
         .args(["target", "list", "--installed"])
-        .output()
-        .expect("failed to execute process");
-    let output = std::str::from_utf8(&output.stdout).unwrap();
-    if !output.lines().any(|line| line == target) {
-        eprintln!("Error: {target} target is not installed. Run `rustup target add {target}`");
-        std::process::exit(1);
+        .output();
+    if let Ok(output) = output {
+        let output = std::str::from_utf8(&output.stdout).unwrap();
+        if !output.lines().any(|line| line == target) {
+            eprintln!("Error: {target} target is not installed. Run `rustup target add {target}`");
+            std::process::exit(1);
+        }   
+
+        // Build test wasm modules
+        cargo_wasi_build(KEYVALUE_TEST_PATH);
+        cargo_wasi_build(HTTP_TEST_PATH);
+        cargo_wasi_build(CONFIGS_TEST_PATH);
     }
 
-    // Build test wasm modules
-    cargo_wasi_build(KEYVALUE_TEST_PATH);
-    cargo_wasi_build(HTTP_TEST_PATH);
-    cargo_wasi_build(CONFIGS_TEST_PATH);
 }
 
 fn cargo_wasi_build(path: &str) {
